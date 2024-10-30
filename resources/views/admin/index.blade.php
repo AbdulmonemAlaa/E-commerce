f354fa197b0b5f7bebc09f2004d08ad70385bb6e
e <abdulmonemalaaaldeen@gmail.com>
2024-10-30 19:06:43 +0300

    Admin and User Authentication

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('content')
+    <h1>Hello From Admin Dashboard</h1>
+@endsection
---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('content')
+    <h1>Hello From User Dashboard</h1>
+@endsection
